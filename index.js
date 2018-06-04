58051955a7b9b6f4d22d22d8c20bc6bc47fc1544
Chase Jensen <soccerchase@gmail.com>
2018-06-04 11:31:07 -0600

    Add Schema for mongoose/mongodb User Schema

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+// Example of Ecmascript 2016 destructuring.
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
