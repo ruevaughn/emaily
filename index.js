bfeba5886062d2d504c11a5e66701376a1eb8857
Chase Jensen <soccerchase@gmail.com>
2018-06-04 00:04:32 -0600

    Commit before refactor

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there buddy!' });
-});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+     console.log('accessToken', accessToken);
+     console.log('refreshToken', refreshToken);
+     console.log('profile:', profile);
+    }
+  )
+);
+
+
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
