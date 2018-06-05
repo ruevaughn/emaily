8d0a0c3f916765cce05057fa991352826b35bf40
Chase Jensen <soccerchase@gmail.com>
2018-06-04 19:31:23 -0600

    URI mismatch but get proxy working between client server

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello there</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign In With Google</a>
       </div>
     );
   }
