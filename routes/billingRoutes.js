bffcfb29b72e0468512d5c98faa3a352794d5b54
Chase Jensen <soccerchase@gmail.com>
2018-06-06 16:57:37 -0600

    Add Heroku postbuild configuration

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,10 +4,6 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({error: 'You must log in!'});
-    }
-
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
