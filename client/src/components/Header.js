264cc38f00fb48f1ddad037c91c8cd47239ab4ff
Chase Jensen <soccerchase@gmail.com>
2018-06-06 16:24:33 -0600

    Style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
-          <li key="3">Credits: {this.props.auth.credits}</li>,
+          <li key="3" style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
           <li key="2"><a href="/api/logout">Logout</a></li>
           ];
     }
