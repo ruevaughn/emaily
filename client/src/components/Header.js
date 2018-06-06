cab9e1f6e304da8af9c7d392d90062fce3b34b8a
Chase Jensen <soccerchase@gmail.com>
2018-06-06 11:11:01 -0600

    Payments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -10,7 +11,10 @@ class Header extends Component {
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li><Payments /></li>,
+          <li><a href="/api/logout">Logout</a></li>
+          ];
     }
   }
 
--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,3 @@ ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
 );
-
-console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY)
-console.log('Environ is', process.env.NODE_ENV)
