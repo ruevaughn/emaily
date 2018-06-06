e3bda663e0ed5efa4beb91067536399b520f1918
Chase Jensen <soccerchase@gmail.com>
2018-06-06 16:02:22 -0600

    Lots of stuffs, add authorization middleware and show / add credits etc.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,6 +21,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
