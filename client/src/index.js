7cb74ff4460db934274e103959a41df8e90b598f
Chase Jensen <soccerchase@gmail.com>
2018-06-06 10:46:10 -0600

    Committing .env files because I don't care.

--- client/src/index.js
+++ client/src/index.js
@@ -12,4 +12,8 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
-  document.querySelector('#root'));
+  document.querySelector('#root')
+);
+
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY)
+console.log('Environ is', process.env.NODE_ENV)
--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,8 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePuslishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
 
