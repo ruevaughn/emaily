de5f049015a5226b326a2f7466a002a600ceecde
Chase Jensen <soccerchase@gmail.com>
2018-06-07 13:54:34 -0600

    Add default action for submitting / emailing survey, wire up but not functional

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,3 +12,7 @@ export const handleToken = (token) => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const submitSurvey = values => {
+  return { type: 'submit_survey ' };
+};
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -19,13 +20,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-
-
       <button
-        className="yellow darken-3 btn-flat"
+        className="yellow darken-3 white-text btn-flat"
         onClick={onCancel}
       >
         Back
+      </button>
+      <button
+        className="green btn-flat white-text right"
+        onClick={() => submitSurvey(formValues)}
+        >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
 
     </div>
@@ -36,4 +42,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
