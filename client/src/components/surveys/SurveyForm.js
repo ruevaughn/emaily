8d7175b6c126f38f3cc1092608e2c59e7157041b
Chase Jensen <soccerchase@gmail.com>
2018-06-06 23:35:18 -0600

    Custom Survey Field

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField'
+
+class SurveyForm extends Component {
+  renderFields() {
+    return(
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+
+          {this.renderFields()}
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm'
 
 class SurveyNew extends Component {
   render() {
     return (
       <div>
-        SurveyNew!
+        <SurveyForm />
       </div>
     );
   }
