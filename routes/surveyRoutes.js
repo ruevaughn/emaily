115444290c5145e4059d6d3204acc093c999163d
Chase Jensen <soccerchase@gmail.com>
2018-06-06 18:40:29 -0600

    Require credits middleware

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,9 @@
-module.exports = app => {
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
+
+module.exports = app => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+  });
 };
