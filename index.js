dd3acf9c2104d48a9baeb6aaeeafc4a3ff9a7ca1
Chase Jensen <soccerchase@gmail.com>
2018-06-06 18:23:30 -0600

    Add Recipient and Survey models and tie surveys to user.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -22,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
