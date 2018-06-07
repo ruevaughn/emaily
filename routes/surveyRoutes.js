a93d2851c0b10585ae6e021cee41324c188bb799
Chase Jensen <soccerchase@gmail.com>
2018-06-07 17:14:06 -0600

    Refactor and cleanup path

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -11,8 +14,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    res.send({});
+   const events = _.map(req.body, ({ email, url }) => {
+    const pathname = new URL(url).pathname;
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    const match = p.test(pathname);
+    if (match) {
+      return {email, surveyId: match.surveyId, choice: match.choice}
+    }
+   });
+
+   const compactEvents = _.compact(events);
+   const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+   res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
