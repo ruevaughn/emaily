f35877295a2372591933c92e10c9a25bfc702512
Chase Jensen <soccerchase@gmail.com>
2018-06-07 00:04:35 -0600

    Style more

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import SurveyField from './SurveyField'
+import SurveyField from './SurveyField';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
@@ -23,12 +24,11 @@ class SurveyForm extends Component {
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
 
           {this.renderFields()}
-
+          <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
           <button type="submit" className="teal btn-flat right white-text">
           Next
           <i className="material-icons right">done</i>
           </button>
-          <button type="submit">Cancel</button>
         </form>
       </div>
     );
