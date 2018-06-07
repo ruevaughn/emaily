5c9f3e615fbfffe359b40c0836ed83bcd2b39f5d
Chase Jensen <soccerchase@gmail.com>
2018-06-07 17:25:23 -0600

    Refactor to use chain in lodash

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -14,18 +14,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-   const events = _.map(req.body, ({ email, url }) => {
-    const pathname = new URL(url).pathname;
-    const p = new Path('/api/surveys/:surveyId/:choice');
-    const match = p.test(pathname);
-    if (match) {
-      return {email, surveyId: match.surveyId, choice: match.choice}
-    }
-   });
+   const events = _.chain(req.body)
+     .map(({ email, url }) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {email, surveyId: match.surveyId, choice: match.choice}
+        }
+     })
+     .compact()
+     .uniqBy('email', 'surveyId')
+     .value();
 
-   const compactEvents = _.compact(events);
-   const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-
+     console.log(events);
    res.send({});
   });
 
