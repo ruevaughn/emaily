0a0bb74b6d74c7dc7a9ab543edc0a012ba4c4538
Chase Jensen <soccerchase@gmail.com>
2018-06-06 23:04:17 -0600

    Start Survey Form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        SurveyNew!
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
