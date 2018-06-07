6753f4a86a730368932ebe05f55b8319ac10776a
Chase Jensen <soccerchase@gmail.com>
2018-06-06 23:47:33 -0600

    Fields showing

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,7 +6,10 @@ class SurveyForm extends Component {
   renderFields() {
     return(
       <div>
-        <Field type="text" name="title" component={SurveyField} />
+        <Field type="text" name="title" label="Survey Title" component={SurveyField} />
+        <Field type="text" name="subject" label="Subject Line" component={SurveyField} />
+        <Field type="text" name="body" label="Email Body" component={SurveyField} />
+        <Field type="text" name="emails" label="Recipient List" component={SurveyField} />
       </div>
     );
   }
