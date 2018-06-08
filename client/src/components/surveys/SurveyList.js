2772b2950569d54eb23b3ac2f78cde4288699d40
Chase Jensen <soccerchase@gmail.com>
2018-06-07 21:08:48 -0600

    Reducer, axios action, display surveys

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card darken-1" keys={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p class="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({surveys}) {
+  return { surveys };
+}
+
+export default connect(mapStateToProps, { fetchSurveys})(SurveyList);
