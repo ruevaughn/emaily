298d5b668fa5f826e68e4bebe4517ce20d7bd66e
Chase Jensen <soccerchase@gmail.com>
2018-06-07 21:36:11 -0600

    Font

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,7 +6,8 @@ const Landing = () => {
       <h1>
         Emaily!
       </h1>
-      Collect feedback from your users
+      Collect feedback from your users!!! SO COOL HOLY SMOKE use 4242 4242 4242 4242
+
     </div>
   );
 };
